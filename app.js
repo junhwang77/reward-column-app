10b1d3f8a4d3733b9c518b45327f2d3336524a69
Jun-Hyun Hwang <junhwang@Jun-Hyuns-MacBook-Pro.local>
2020-05-24 00:05:12 -0700

    Change app.js

--- app.js
+++ app.js
@@ -51,5 +51,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(process.env.PORT || 9000, 
+	() => console.log("Server is running..."));
+
 
 module.exports = app;
