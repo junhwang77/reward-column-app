b4e296d93e95b96653c48dc5e48407a7a51490d0
Jun-Hyun Hwang <junhwang@Jun-Hyuns-MacBook-Pro.local>
2020-05-24 02:31:09 -0700

    Fix frontend path

--- app.js
+++ app.js
@@ -53,10 +53,10 @@ app.use(function(err, req, res, next) {
 
 const path = require('path')
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'reward-column-app/build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  res.sendFile(path.join(__dirname + '/reward-column-app/build/index.html'))
 })
 
 module.exports = app;
