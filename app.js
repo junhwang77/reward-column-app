08963612a7d636b1d0fdcfe0f3a556802b573f33
Jun-Hyun Hwang <junhwang@Jun-Hyuns-MacBook-Pro.local>
2020-05-24 03:13:18 -0700

    fix default path

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 var cors = require("cors");
 
 //require routes***
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
 var positions = require('./routes/positions-router')
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Add routes here***
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
 app.use("/positions", positions);
