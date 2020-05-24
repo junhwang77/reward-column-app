4eb72550039600df3f2f6d128d72c2e14174a4f6
Jun-Hyun Hwang <junhwang@Jun-Hyuns-MacBook-Pro.local>
2020-05-24 02:18:28 -0700

    Add front-end

--- app.js
+++ app.js
@@ -51,4 +51,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 module.exports = app;
