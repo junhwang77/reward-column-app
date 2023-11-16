d0daf65921b9573b9e6872f7933f27066ebf19e2
Jun Hwang <junhwang77@hotmail.com>
2023-11-15 22:12:25 -0800

    Add build files for render

---
+++ reward-column-app/build/precache-manifest.bcc349d4c160572887fb7fc0585d734c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649766a3be90a408e3f2a96dd195193d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe4424a8d6277c18793",
+    "url": "/static/js/2.5937fd52.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.5937fd52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94f707877b3c625279d",
+    "url": "/static/js/main.7fed2817.chunk.js"
+  },
+  {
+    "revision": "6c8a8afe2bd4130ea72f",
+    "url": "/static/js/runtime-main.e1137d3d.js"
+  }
+]);
